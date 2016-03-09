52b555b165e0124c0e64c6bd0d4b4b32311cd1e5
Sarah Hamdi <s.hamdi@gmail.com>
2016-03-08 22:23:13 -0500

    Added custom content + got some of it on the page

--- page.php
+++ page.php
@@ -4,16 +4,77 @@
   <div class="container">
 
     <div class="content">
-      <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+      <!-- start personal statement loop -->
+      
+      <?php $personalStatement = new WP_Query(array(
+        'posts_per_page'=> 1,
+        'post_type'=> 'personal_statement'
+      )); ?>
 
-      <?php endwhile; // end the loop?>
+      <?php if ($personalStatement-> have_posts()): ?>
+        <?php while ($personalStatement-> have_posts()): ?>
+          
+        <?php endwhile; ?>
+      <?php endif; ?>
+
+
+
+    <!-- start portfolio content loop -->
+
+      <?php $Portfolio = new WP_Query(array(
+        'posts_per_page'=> -1,
+        'post_type'=> 'portfolio'
+      )); ?>
+
+      <?php if ($Portfolio-> have_posts()): ?>
+        <?php while ($Portfolio-> have_posts()): ?>
+          <?php $Portfolio-> the_post(); ?>
+            <!-- project name -->
+            <h3><?php echo get_field('project_name'); ?></h3>
+            <!-- project description -->
+            <p><?php echo get_field('project_description'); ?></p>
+            <!-- images -->
+            <?php $image = get_field('image_bw'); ?>
+            <img src="<?php echo $image['sizes']['medium'] ?>" alt="">
+            <?php $image = get_field('image_colour'); ?>
+            <img src="<?php echo $image['sizes']['medium']; ?>" alt="">
+            <!--  project links-->
+            <?php while(has_sub_field('project_links')): ?>
+              <?php $links = get_field('project_links'); ?>
+                <p>
+                  <a href="<?php echo $links['0']['project_link']; ?>"><?php echo $links['0']['project_link_title']; ?></a>
+                </p>
+          <?php endwhile; ?> <!-- end project link loop -->     
+        <?php endwhile; ?> <!-- end portfolio content printing loop -->
+      <?php endif; ?> <!-- end portfolio loop -->
+
+
+      <!-- start blog posts loop  -->
+
+      <?php $Blog = new WP_Query(array(
+        'posts_per_page'=> -1,
+        'post_type'=> 'blog_posts'
+      )); ?>
+
+      <?php if ($Blog-> have_posts()): ?>
+        <?php while ($Blog-> have_posts()): ?>
+          <?php $Blog-> the_post(); ?>
+            <?php $blogInfo = get_field('blog_info'); ?>
+              <ul>
+                <!-- loops through all the items in the blog_info array and returns titles, links & publications -->
+                <?php 
+                  for ($x = 0; $x < count($blogInfo); $x++) {
+                     echo "<li>" . "<a href='" . $blogInfo[$x]['blog_link'] . "'>" . $blogInfo[$x]['blog_title'] . "</a></li>";
+                     echo "<p>" . $blogInfo[$x]['blog_publication'];
+                  }
+                ?>
+              </ul>
+        <?php endwhile; ?> <!-- end while loop for blog posts -->
+      <?php endif; ?> <!-- end blog loop -->
+      
+
     </div> <!-- /,content -->
-
-    <?php get_sidebar(); ?>
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
